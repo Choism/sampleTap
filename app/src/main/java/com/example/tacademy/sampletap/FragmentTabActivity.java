b2bf337adaf4628184002c398335ac627ef9415e
Seok Min Choi <pcgo118@gmail.com>
2016-07-25 16:25:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tacademy/sampletap/FragmentTabActivity.java
@@ -0,0 +1,58 @@
+package com.example.tacademy.sampletap;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
+import android.widget.Toast;
+
+public class FragmentTabActivity extends AppCompatActivity {
+
+    FragmentTabHost tabHost;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_tab);
+        tabHost = (FragmentTabHost)findViewById(R.id.tabhost);
+        tabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("TAB1"),
+                OneFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("TAB2"),
+                TwoFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("TAB3"),
+                ThreeFragment.class, null);
+
+
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String s) {
+                if ("tab2".equals(s)) {
+                    Toast.makeText(FragmentTabActivity.this, "tab2 click", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        tabHost.setCurrentTabByTag("tab2");
+    }
+
+
+    public void receiveText(String text) {
+        Fragment f = getSupportFragmentManager()
+                .findFragmentByTag("tab3");
+        if (f != null) {
+            ((ThreeFragment)f).setMessage(text);
+        } else {
+            savedMessage = text;
+        }
+        tabHost.setCurrentTabByTag("tab3");
+    }
+
+    public String getSavedMessage() {
+        return savedMessage;
+    }
+
+    String savedMessage = null;
+
+}
---
+++ app/src/main/java/com/example/tacademy/sampletap/ThreeFragment.java
@@ -0,0 +1,41 @@
+package com.example.tacademy.sampletap;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ThreeFragment extends Fragment {
+
+
+    public ThreeFragment() {
+        // Required empty public constructor
+    }
+
+    TextView messageView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_three, container, false);
+        messageView = (TextView)view.findViewById(R.id.textView7);
+        return view;
+    }
+
+    public void setMessage(String text) {
+        if (messageView != null) {
+            messageView.setText(text);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        String message = ((FragmentTabActivity)getActivity()).getSavedMessage();
+        if (message != null) {
+            messageView.setText(message);
+        }
+    }
+}
